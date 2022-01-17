8d83c4cbeae812c7cc0da7b2094b99b85429545c
sdc-3046 <chavanshubham879@gmail.com>
2022-01-17 23:21:39 +0530

    First Commit

---
+++ SQL/-- CRUD OPERATIONS.sql
@@ -0,0 +1,27 @@
+-- CRUD OPERATIONS
+
+-- Creating a table
+-- CREATE TABLE emtecinterns(id INT IDENTITY PRIMARY KEY, name varchar(20), city varchar(10), state varchar(20), batch INT);
+
+-- Reading the table
+-- SELECT * from emtecinterns;
+
+-- Inserting the values in table
+-- INSERT INTO emtecinterns VALUES('ABC','PUNE','MH',2022);
+-- INSERT INTO emtecinterns VALUES('DEF','SANGLI','MH',2022);
+-- INSERT INTO emtecinterns VALUES('GHI','KOLHAPUR','MH',2021);
+-- INSERT INTO emtecinterns VALUES('JKL','AURANGABAD','MH',2022);
+-- INSERT INTO emtecinterns VALUES('MNO','NAGPUR','MH',2021);
+-- INSERT INTO emtecinterns VALUES('PQR','MUMBAI','MH',2022);
+
+-- Update the values in table
+-- UPDATE emtecinterns SET name='XYZ' WHERE id=3;
+-- UPDATE emtecinterns SET batch=2022;
+
+-- delete values from table
+-- DELETE from emtecinterns WHERE name='XYZ';
+-- DELETE emtecinterns;
+
+-- Reading the table
+-- SELECT * FROM emtecinterns;
+
